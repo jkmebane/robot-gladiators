3f404e69ef13c31f4520c78b67b143a412a62f7e
Keezy <jkmebane@gmail.com>
2023-07-11 10:09:41 -0400

    using console devtools

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,19 @@
 var playerName = window.prompt("What is your robot's name?");
 
+//Note the lack of quotation marks around playerName
+
+//What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message")
+
+// this will do math and log 20 
+console.log(10 + 10);
+
+//WHAT IS THIS?
+console.log("Our robot's name is " + playerName);
+
 //This creates a funtion named "fight"
 function fight () {
 window.alert("The fight has begun!");
 }
-fight();
+//fight();
